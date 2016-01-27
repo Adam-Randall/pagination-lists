1ef1837fd1743087a1878b902ed79694690a4889
Adam Randall <ajp.randall@gmail.com>
2016-01-27 22:13:14 +1300

    Model: Paginator model updated as is controller

--- app/controllers/paginator_controller.rb
+++ app/controllers/paginator_controller.rb
@@ -1,12 +1,30 @@
 class PaginatorController < ApplicationController
   
   def index
-    paginate(page_number, params[:numbers_per_page])
+  	if paginator_params[:numbers_per_page].present?
+  		@number_per_page = paginator_params[:numbers_per_page].to_i 
+  	else
+  		@number_per_page = 1000
+  	end
+
+  	if paginator_params[:current_page].present?
+  		current_page = paginator_params[:current_page].to_i 
+  	else
+  		current_page = 0
+  	end
+
+  	@pages = Paginator.pagination_to_display(@number_per_page, current_page)
+
+    @numbers = Paginator.numbers_as_words(current_page,	@number_per_page)
+
+    @total_pages = Paginator.total_page_count(@number_per_page)
   end
 
   private
 
   def paginator_params
-    params.permit(:numbers_per_page)
+    params.permit(:numbers_per_page, :current_page)
   end
+
+ 
 end
--- app/models/paginator.rb
+++ app/models/paginator.rb
@@ -2,21 +2,53 @@ class Paginator
 
   MAX_COUNT = 1000000 #One Million
 
-  def self.paginate(page_number, numbers_per_page)
-    first_number = page_number * numbers_per_page
+  def self.numbers_as_words(page_number, numbers_per_page)
+    first_number = (page_number * numbers_per_page) + 1
     last_number = first_number + numbers_per_page
-    numbers = first_number.upto(last_number)
+
+    last_number = MAX_COUNT if last_number > MAX_COUNT
+
+    numbers = first_number.upto(last_number).map{|n| n = NumberToWord.to_words(n)}
   end
 
-  def total_page_count(numbers_per_page)
+
+
+  def self.pagination_to_display(numbers_per_page, page_number)
+    numbers = []
+    
+    page_count = total_page_count(numbers_per_page)
+
+    if page_number > 5
+      first_number = page_number - 5
+    else
+      first_number = 1
+    end
+
+    if page_count-2 <= page_number + 5
+      last_number = page_count-2
+    else
+      last_number = page_number + 5
+    end
+
+    numbers << {key: 0, value: 0}
+
+    first_number.upto(last_number).each_with_index do | number, index |
+      numbers << {key: index+1, value: number}
+    end
+
+    if numbers.count != 11
+      number_to_move = numbers.pop
+      numbers << {key: 11, value: number_to_move[:value]}
+    end
+
+    numbers << {key: 12, value: page_count-1}
+
+    return numbers
+  end
+
+  def self.total_page_count(numbers_per_page)
     page_count = MAX_COUNT / numbers_per_page
     (MAX_COUNT % numbers_per_page > 0) ? (return page_count+1) : (return page_count)
-  end
-
-  def next_page(current_page)
-  end
-
-  def previous_page(current_page)
   end
 
 end
