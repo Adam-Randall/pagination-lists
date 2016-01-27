62828515125ac65d40fcbf0c1237f7df61a54756
Adam Randall <ajp.randall@gmail.com>
2016-01-27 15:42:33 +1300

    Test: Paginator Model Test (WIP)

---
+++ test/models/paginator_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class PaginatorTest# < ActiveSupport::TestCase
+
+
+
+  describe "Page Can Calculate Number of Pages" do
+    it "pages succesfully calculated" do
+      #binding.pry
+    end
+
+  end
+end
